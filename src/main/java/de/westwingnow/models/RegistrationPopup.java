238a365e267ef765e12e5d1f73f1ad097149d993
George POSTELNICU <george.postelnicu@ext.ec.europa.eu>
2021-01-25 18:18:32 +0100

    Login screen 1/x.

--- src/main/java/de/westwingnow/models/RegistrationPopup.java
+++ src/main/java/de/westwingnow/models/RegistrationPopup.java
@@ -13,11 +13,18 @@ public class RegistrationPopup extends SeleniumBase {
 	private static final By SWITCH_TO_LOGIN_LOCATOR = By.xpath(".//button[@data-testid='login-button']");
 	private static final By POPUP_PROMO_LOCATOR = By.xpath(".//div[@data-identifier='SHOP_TO_CLUB_REGISTRATION_SINGLE_STEP_OVERLAY']");
 	private static final By DISMISS_POPUP_LOCATOR = By.xpath(".//button[@data-identifier='LOGIN_AND_REGISTER']");
+	private static final By EMAIL_LOCATOR = By.xpath(".//input[@data-testid='long-register-email-field']");
+	private static final By PASS_LOCATOR = By.xpath(".//input[@data-testid='long-register-password-field']");
+	private static final By LOGIN_LOCATOR = By.xpath(".//button[@data-testid='login_reg_submit_btn']");
 	private final WebElement backdrop;
 
 	public RegistrationPopup(WebDriver driver, WebElement backdrop) {
 		super(driver);
 		this.backdrop = backdrop;
+	}
+
+	public void login() {
+
 	}
 
 	public void dismiss() {
